bd4b92fc4f1b834375195c69f3e2c077008b54f4
Binaaa04 <sabrinarahmadini60@gmail.com>
2023-12-09 19:00:58 +0700

    Modify Array Number 21

--- ArrayNumber21.java
+++ ArrayNumber21.java
@@ -5,15 +5,9 @@
 public class ArrayNumber21 {
 
     public static void main(String[] args) {
-        int [] num = new int[4];
-        num[0]=5;
-        num[1]=12;
-        num[2]=7;
-        num[3]=20;
-
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+        int num []= {5, 12, 7, 20};
+            for (int i = 0; i <= 4; i++) {
+            System.out.println( num [i]);   
     }
+}
 }
